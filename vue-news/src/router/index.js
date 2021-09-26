fed1902b0b018a9a226b73de56783ccbe9742899
sjkim-jinnyk <sjkim.jinnyk@gmail.com>
2021-09-26 17:07:41 +0900

    Router 설치 및 구현

---
+++ vue-news/src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import AskView from '../views/AskView.vue';
+import NewsView from '../views/NewsView.vue';
+import JobsView from '../views/JobsView.vue';
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+    routes: [
+        {
+            path: '/news',
+            component: NewsView,
+        },
+        {
+            path: '/ask',
+            component: AskView,
+        },
+        {
+            path: '/jobs',
+            component: JobsView,
+        },
+    ]
+});
