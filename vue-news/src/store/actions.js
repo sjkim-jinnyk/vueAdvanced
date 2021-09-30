7b05c68f4d2c3b9a39ae15f440a0dd5d18d58b88
sjkim-jinnyk <sjkim.jinnyk@gmail.com>
2021-09-30 11:08:49 +0900

    데이터 정리 및 스타일링

--- vue-news/src/store/actions.js
+++ vue-news/src/store/actions.js
@@ -4,7 +4,7 @@ export default {
     FETCH_NEWS(context) {
         fetchNewsList()
             .then( response => {
-                console.log(response.data);
+                //console.log(response.data);
                 context.commit('SET_NEWS', response.data);  // mutation에 데이터 넘기기
             })
             .catch(error => console.log(error));
@@ -26,7 +26,10 @@ export default {
     },
     FETCH_ASK(context) {
         fetchAskList()
-            .then(response => context.commit('SET_ASK', response.data))
+            .then(response => {
+                console.log(response.data);
+                context.commit('SET_ASK', response.data)
+            })
             .catch(error => console.log(error));
     }
 }
--- vue-news/src/views/JobsView.vue
+++ vue-news/src/views/JobsView.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-      <div v-for="job in this.$store.state.jobs" :key="job.id">{{ job.title }}</div>
+      <p v-for="item in this.$store.state.jobs" :key="item.id">
+        <a :href="item.url">{{ item.title }}</a>
+        <small>{{ item.time_ago }}, {{ item.domain }}</small>
+      </p>
   </div>
 </template>
 
